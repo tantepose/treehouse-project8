c7fe319c757e30052827805c6a77dfea25e1f22d
Ole Petter Baugerød Stokke <doktoresel@gmail.com>
2018-03-26 22:10:54 +0200

    most functionality in place

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,143 @@
+/*****************************************
+Treehouse Fullstack Javascript Techdegree,
+project #8: "Using Gulp to Build a Front End Website"
+by Ole Petter Baugerød Stokke
+www.olepetterstokke.no/treehouse/project8
+******************************************/
+
 'use strict';
 
-//all the modules we need:
+/*****************************************
+    SETUP
+*****************************************/
+
+// all modules required
 var gulp = require('gulp'); //Gulp itself
-var concat = require('gulp-concat'); //concatinate files
-var uglify = require('gulp-uglify'); //minification of Javascript
-var rename = require('gulp-rename'); //renaming files
+var webserver = require('gulp-webserver'); //the webserver
+var runSequence = require('run-sequence'); //run tasks in sequence
+
+var concat = require('gulp-concat'); //concat Javascript
+var uglify = require('gulp-uglify'); //minify Javascript
+var maps = require('gulp-sourcemaps'); //generating sourcemaps, for both JS and sass
+
 var sass = require('gulp-sass'); //sass to CSS
-var maps = require('gulp-sourcemaps'); //generating sourcemaps
+var cleanCSS = require('gulp-clean-css'); //minify css
+
+var image = require('gulp-image'); //optimize images
+var rename = require('gulp-rename'); //renaming files
 var del = require('del'); //deleting files
 
+//the dist folders
+var dist = {
+    'content': 'dist/images',
+    'styles': 'dist/css',
+    'scripts': 'dist/js'
+}
+
+/*****************************************
+    JAVASCRIPT
+*****************************************/
+
+// concat all JS-files to one
+gulp.task('concatScripts', function () {
+    return gulp.src(['js/circle/autogrow.js', //get .js-files
+        'js/circle/circle.js',
+        'js/global.js'])
+    .pipe(maps.init()) //make sourcemap
+    .pipe(concat('all.js')) //concat them
+    .pipe(maps.write('./')) //save sourcemap
+    .pipe(gulp.dest(dist.scripts)) //save all.js
+});
+
+// take the concatinated all.js file and minify it
+gulp.task('scripts', ['concatScripts'], function () {
+    return gulp.src(dist.scripts + '/all.js') //get js
+    .pipe(uglify()) //minify it
+    .pipe(rename('all.min.js')) //rename it
+    .pipe(gulp.dest(dist.scripts)); //save it
+});
+
+/*****************************************
+    SASS
+*****************************************/
+
+//compile all sass-files to one CSS file
+gulp.task('compileSass', function () {
+    return gulp.src('sass/global.scss') //get sass
+    .pipe(maps.init()) //make map
+    .pipe(sass()) //compile sass
+    .pipe(maps.write('./')) //save map
+    .pipe(gulp.dest(dist.styles)); //save css
+});
+
+//minify the CSS
+gulp.task('styles', ['compileSass'], function () {
+    return gulp.src(dist.styles + '/global.css') //get css
+    .pipe(cleanCSS({compatibility: 'ie8'})) //minify it
+    .pipe(rename('all.min.css')) //rename it
+    .pipe(gulp.dest(dist.styles)); //save it
+});
+
+/*****************************************
+    IMAGES
+*****************************************/
+
+//optimize images
+gulp.task('images', function () {
+    return gulp.src('images/*') //get images
+    .pipe(image()) //optimize them
+    .pipe(gulp.dest(dist.content)); //save them
+});
+
+/*****************************************
+    FILES
+*****************************************/
+
+//delete dist folder
+gulp.task('clean', function () {
+    return del(['dist']); //delete folder
+});
+
+//copy static files (index.html and icons folder)
+gulp.task('copyStaticFiles', function () {
+    return gulp.src([  'index.html', //files to copy
+                'icons/**'], 
+                { base: './' }) //keep folder structure
+    .pipe(gulp.dest('dist')); //copy to dist folder
+});
+
+/*****************************************
+    BUILD
+*****************************************/
+
+//build task - run all build tasks
+gulp.task('build', ['clean'], function () { //clean will run first
+    gulp.start('copyStaticFiles');
+    gulp.start('scripts');
+    gulp.start('styles');
+    gulp.start('images');
+});
+
+//start webserver with live reload
+gulp.task('serve', function () {
+    gulp.src('dist') //start server from dist folder
+    .pipe(webserver({
+      livereload: true,
+      directoryListing: false,
+      open: true
+    }));
+});
+
+// watch for changes in sass
+gulp.task('watch', function () {
+    gulp.watch('sass/**/*.scss', ['styles']); //run 'styles' if changes to sass files
+});
+
+//default - all tasks are run in sequence to avoid trouble
+gulp.task('default', function () {
+    runSequence(
+        'clean',
+        ['copyStaticFiles', 'scripts', 'styles', 'images'], //running async
+        'serve',
+        'watch');
+});
--- readme.md
+++ readme.md
@@ -17,25 +17,26 @@ The build process must fulfill the following criteria:
 
 To complete this project, follow the instructions below:
 
-* As a developer, I should be able to run the npm install command to install all of the dependencies for the build process.
+* X As a developer, I should be able to run the npm install command to install all of the dependencies for the build process.
 
-* As a developer, I should be able to run the gulp scripts command at the command line to concatenate, minify, and copy all of the project’s JavaScript files into an all.min.js file that is then copied to the dist/scripts folder.
+* X As a developer, I should be able to run the gulp scripts command at the command line to concatenate, minify, and copy all of the project’s JavaScript files into an all.min.js file that is then copied to the dist/scripts folder.
 
-* As a developer, I should be able to run the gulp styles command at the command line to compile the project’s SCSS files into CSS, then concatenate and minify into an all.min.css file that is then copied to the dist/styles folder.
+* X As a developer, I should be able to run the gulp styles command at the command line to compile the project’s SCSS files into CSS, then concatenate and minify into an all.min.css file that is then copied to the dist/styles folder.
 
-* As a developer, when I run the gulp scripts or gulp styles commands at the command line, source maps are generated for the JavaScript and CSS files respectively.
+* X As a developer, when I run the gulp scripts or gulp styles commands at the command line, source maps are generated for the JavaScript and CSS files respectively.
 
-* As a developer, I should be able to run the gulp images command at the command line to optimize the size of the project’s JPEG and PNG files, and then copy those optimized images to the dist/content folder.
+* X As a developer, I should be able to run the gulp images command at the command line to optimize the size of the project’s JPEG and PNG files, and then copy those optimized images to the dist/content folder.
 
-* As a developer, I should be able to run the gulp clean command at the command line to delete all of the files and folders in the dist folder.
+* X As a developer, I should be able to run the gulp clean command at the command line to delete all of the files and folders in the dist folder.
 
-* As a developer, I should be able to run the gulp build command at the command line to run the clean, scripts, styles, and images tasks with confidence that the clean task completes before the other commands.
+* X As a developer, I should be able to run the gulp build command at the command line to run the clean, scripts, styles, and images tasks with confidence that the clean task completes before the other commands.
 
-* As a developer, I should be able to run the gulp command at the command line to run the build task and serve my project using a local web server.
+* X As a developer, I should be able to run the gulp command at the command line to run the build task and serve my project using a local web server.
 
 ---
 
 To get an "exceeds" rating, you can expand on the project in the following ways:
 
-* As a developer, when I run the default gulp command, it should continuously watch for changes to any .scss file in my project. When there is a change to one of the .scss files, the gulp styles command is run and the files are compiled, concatenated, and minified to the dist folder. 
-* My project should then reload in the browser, displaying the changes.
+* X As a developer, when I run the default gulp command, it should continuously watch for changes to any .scss file in my project. When there is a change to one of the .scss files, the gulp styles command is run and the files are compiled, concatenated, and minified to the dist folder. 
+
+* X My project should then reload in the browser, displaying the changes.
